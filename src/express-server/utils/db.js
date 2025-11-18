6b0a0fc8bb69a650f213903038c374449116c493
Ricardo Dias <ricardosantosdias06@gmail.com>
2025-11-18 00:08:17 +0000

    Criação das queries no SQL e testes usando o Postman

--- src/express-server/utils/db.js
+++ src/express-server/utils/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host:'localhost', user:'root', password: '0000', database:'champions_league',  
+    host:'localhost', user:'root', password: 'root', database:'champions_league',  
 });
 
 connection.connect((err) => {
