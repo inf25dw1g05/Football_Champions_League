9d8c3908e0385c748e0dae3d4b9c4eaeb5746f56
Rodrigo Esteves <rodrigo.m.esteves06@gmail.com>
2025-11-20 12:21:29 +0000

    docker a funcionar localmente, falta enviar a imagem ae criar docker-compose.prod para correr a imagem do docker hub

--- src/express-server/utils/db.js
+++ src/express-server/utils/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host:'localhost', user:'root', password: 'root', database:'champions_league',  
+    host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME,  
 });
 
 connection.connect((err) => {
